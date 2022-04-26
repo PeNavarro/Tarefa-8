f7c758e65e1e64df4962a9cf6a191acdc40aa35a
Pedro Mack Navarro <pemnavarro@hotmail.com>
2022-04-26 11:03:22 -0300

    Criação componente produto

---
+++ src/app/mock-products.ts
@@ -0,0 +1,9 @@
+import {Product} from './product'
+
+export const PRODUCTS: Product[] = [
+      { id: 1, name: 'Sabão', price: 10, description: 'Sabão para lavar as roupas', category: 'Casa'},
+      { id: 2, name: 'Pão de alho', price: 5, description: 'Pão de alho Ceara', category: 'Churrasco'},
+      { id: 3, name: 'Refrigerante', price: 8, description: 'Coca-cola 2 litros', category: 'Bebidas'},
+      { id: 4, name: 'Shampoo', price: 12, description: 'Shampoo Dove 2 em 1', category: 'Higiene'},
+      { id: 5, name: 'Maça', price: 2, description: 'Maça da fazenda do seu Zé', category: 'Comidas'},
+];
